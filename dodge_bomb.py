3d8a48d10d8daff93d76005dd19d24aa38e05017
konishiat <c0a240923a@edu.teu.ac.jp>
2025-06-17 15:47:26 +0900

    追加機能2完成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,8 +16,6 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     bb_img = pg.Surface((20, 20))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_img.set_colorkey((0, 0, 0))
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     bb_rct = bb_img.get_rect()
@@ -41,9 +39,14 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-
         kk_rct.move_ip(sum_mv)
-        #bb_rct.move_ip(vx, vy)
+
+        bb_imgs, bb_accs = init_bb_imgs()
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        avy = vy*bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_img.set_colorkey((0, 0, 0))
+        bb_rct.move_ip(avx, avy)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         yoko, tate = check_bound(bb_rct)
@@ -51,11 +54,13 @@ def main():
             vx *= -1
         if not tate:
             vy *= -1
+
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
 
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
@@ -69,6 +74,7 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
     return yoko, tate
+
 
 def gameover(screen: pg.Surface) -> None:
     """
@@ -87,9 +93,24 @@ def gameover(screen: pg.Surface) -> None:
     fonto = pg.font.Font(None, 80)
     txt = fonto.render("Game Over", True, (255, 255, 255))
     screen.blit(txt, [400, 300])
-    
+
     pg.display.update()
     time.sleep(5)
+
+
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    サイズの異なる爆弾Surfaceを要素としたリストと
+    加速度リストを返す
+    """
+    bb_accs = [a for a in range(1, 11)]
+    bb_imgs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_imgs.append(bb_img)
+    return tuple(bb_imgs), tuple(bb_accs)
+
 
 if __name__ == "__main__":
     pg.init()
